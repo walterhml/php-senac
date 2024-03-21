a1767159e35c381074312ca537d1abd1b42457ef
walterhml <wszin06@gmail.com>
2024-03-21 19:57:26 -0300

    alterando estruturas

--- 01/teste.php
+++ 01/teste.php
@@ -8,7 +8,7 @@ echo '<h1>ola Mundo!</h1>';
 /*
 comentario de varias linhas
 */
-
+echo "<hr>";
 // declaração de variaveis
 $nome = 'joao';
 $sobrenome = 'silva';
@@ -19,7 +19,7 @@ $nome_comleto = $nome . " " . $sobrenome;
 
 echo $nome . " " . $sobrenome . "<br>";
 echo $nome_comleto . '<br>';
-
+echo "<hr>";
 // variaveis numericas
 $idade = 25 . '<br>';
 $ano = 2024 . '<br>';
@@ -29,11 +29,24 @@ echo $ano - $idade;
 
 echo 'esse é um texto em aspas simples <br>';
 echo "esse é um texto com aspas duplas <br>";
-
+echo "<hr>";
 //trabalhando  com aspas simples e duplas
 echo 'dom pedro disse: "independencia ou morte! <br>';
 
 echo "$nome tem $idade anos de idade <br>";
 echo '$nome tem $idade anos de idade <br>';
+echo "<hr>";
+
+//testando o case sensitive - palavras reservadas
+echo "Texto 1 <br>";
+ECHO "Texto 2 <br>";
+echo "<hr>";
+// testando o case sensitive - variaveis
+$primeiroNome = "Carlos";
+
+echo $primeiroNome;
+ECHO $PRIMEIRONOME;
+
+
 
 ?>
