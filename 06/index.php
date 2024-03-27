764ad187b049ba6eece3681d7f0ce75a19f705b2
Waltheer__ <135153366+walterhml@users.noreply.github.com>
2024-03-27 20:46:40 -0300

    aula 06 completed

--- 06/index.php
+++ 06/index.php
@@ -42,7 +42,7 @@ print_r($array_convertido);
 # exemplo adicional conversão para array
 $hora = "20:02:56"; //hora lida de um log
 $array_hora_minuto = explode(":", $hora);
-echo "O evento ocorreu às $array_hora_minuto[0] horas, $array_hora_minuto[1] minutos, $array_hora_minuto[2] segundos.";
+echo "O evento ocorreu às $array_hora_minuto[0] horas, $array_hora_minuto[1] minutos, $array_hora_minuto[2] segundos. <br>";
 
 # exemplo adicional 2 conversao do array
 $contatos = "Aecio-Nerildo-JS-Celso-Luana";
@@ -50,6 +50,39 @@ echo "A lista de contatos é: $contatos <br>";
 
 $contatos_array = explode("-", $contatos);
 print_r($contatos_array);
+echo "<br>";
+
+// palavra reservadas e funções comuns
+echo "Exemplo de uso da função stroupper para converter uma string em maiscula: " . strtoupper($contatos) . "<br>";
+
+echo "Exemplo de uso da palavra reservada require para incluir um arquivo: ";
+// require 'arquivo.php'; // comentar para nao pedir error
+echo "<br>";
+
+echo "exemplo de uso de palavra reservada private para definir um metodo privado em uma classe: ";
+echo "<br>";
+
+class MinhaClasse {
+    private $atributo;
+
+    private function metodoPrivado() {
+    // implementação do metodo
+    }
+}
+
+echo "<br>";
+echo "Exemplo de uso da constante magica __DIR__ para obter o diretorio atual: " . __DIR__ . "<br>";
+
+echo "<br>";
+
+echo "Exemplo de uso da constane magica __FILE__ para obter o caminho completo do arquivo atual: " . __FILE__ . "<br>";
+
+
+
+
+
+
+
 
 
 
