ed829d220e97af02d619cd16ef9402295a7ce8bd
Waltheer__ <135153366+walterhml@users.noreply.github.com>
2024-04-08 21:26:51 -0300

    conexão

--- 14/01_conexao.php
+++ 14/01_conexao.php
@@ -1,11 +1,17 @@
 <?php
 
 // conexão
-    $connection = new mysqli("localhost", "root", "", );
+$connection = new mysqli("localhost", "root", "", "contatos");
 
+// verificando a conexão
+if (!$connection->connect_errno) {
+    echo "Sucesso na conexão! <br>";
+}
 
-
-
+if ($connection->connect_errno) {
+    echo "Erro de conexão! <br>";
+    echo "erro: " . mysqli_connect_error();
+}
 
 
 
