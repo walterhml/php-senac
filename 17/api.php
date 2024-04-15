65eef24b17cf4e8f236a775a4ed93d9c9e8114cc
Waltheer__ <135153366+walterhml@users.noreply.github.com>
2024-04-15 20:57:55 -0300

    starting database with class static

--- 17/api.php
+++ 17/api.php
@@ -2,9 +2,9 @@
 
 require_once 'DatabaseRepository.php';
 
-print_r(DatabaseRepository::getAllContacts());
+//print_r(DatabaseRepository::getContactbyId());
 
-
+echo DatabaseRepository::deleteContact(22, "Pessa B", "11 99755555", "pessoa_b@mail.com");
 
 
 
