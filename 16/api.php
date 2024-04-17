089ae88b1c0f73e592478bb63a6d901528b0c99e
Waltheer__ <135153366+walterhml@users.noreply.github.com>
2024-04-17 19:56:09 -0300

    aula 17

--- 16/api.php
+++ 16/api.php
@@ -1,28 +1,37 @@
 <?php
 require_once 'DatabaseRepository.php';
 
-$repository = new DatabaseRepository();
-$action = isset($_GET['action']) ? $_GET['action'] : '';
+$action = $_GET['action'];
 
 switch($action) {
     case 'list':
-        echo json_encode($repository->getAllItems());
+        echo json_encode(DatabaseRepository::getAllContacts());
+        break;
+    case 'get':
+        $id = $_GET['id'];
+        echo json_encode(DatabaseRepository::getContactById($id));
+        break;
+    case 'getNome':
+        $nome = $_GET['nome'];
+        echo json_encode(DatabaseRepository::getContactByName($nome));
         break;
     case 'add':
         $data = json_decode(file_get_contents('php://input', true));
-        $nomeProduto = $data->nome_produto;
-        $quantidade = $data->quantidade;
-        echo json_decode($repository->addItem($nomeProduto, $quantidade));
+        $success = DatabaseRepository::insertContact($data->nome, $data->telefone, $data->email);
+        echo json_encode(['scuccess' => $success]);
         break;
-    case 'compraItem':
+    case 'update':
         $data = json_decode(file_get_contents('php://input', true));
-        $id = $data->id;
-        $comprado = $data->comprado;
-        echo json_decode($repository->compraItem($id, $comprado));
+        $success = DatabaseRepository::updateContact($data->id, $data->nome, $data->telefone, $data->email);
+        echo json_encode(['success' => $success]);
         break;
     case 'delete':
         $id = $_GET['id'];
-        echo json_decode($repository->deleteItem($id));
+        $success = DatabaseRepository::deleteContact($id);
+        echo json_encode(['success' => $success]);
         break;
+    default:
+        echo json_encode(['error' => 'Ação inválida']);
 }
+
 ?>
