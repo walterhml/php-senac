a1491ce3f637c08daba4640bf7a5304360d48838
Walther <wszin06@gmail.com>
2024-04-18 20:59:21 -0300

    att

--- 17/api/cliente_api.php
+++ 17/api/cliente_api.php
@@ -8,6 +8,17 @@ switch($action) {
     case 'listar':
         listarClientes();
         break;
+    case 'buscar':
+        buscarClientePorId();
+        break;
+    case 'cadastrar':
+        cadastrarCliente();
+        break;
+    case 'atualizar':
+        atualizarCliente();
+    case 'excluir':
+        excluirCliente();
+        break;
     default:
         http_response_code(400); // Requisição inválida
         echo json_encode(['error' => 'Ação inválida']);
@@ -17,3 +28,24 @@ function listarClientes() {
     $clientes = ClienteRepository::getAllClientes();
     echo json_encode($clientes);
 }
+
+function buscarClientePorId() {
+
+}
+
+function cadastrarCliente() {
+
+}
+
+function atualizarCliente() {
+
+}
+
+function excluirCliente() {
+
+}
+// Create Read Update Ddelete
+// Create = POST (INSERT)
+// Read = GET (SELECT)
+// Update = POST(GET) (UPDATE SET)
+// Delete = POST (DELETE)
