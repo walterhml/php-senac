541ef574543cc589148569cb46799889e9d31bc7
Walther <wszin06@gmail.com>
2024-04-23 20:49:28 -0300

    18

---
+++ 18/api/conta_api.php
@@ -0,0 +1,95 @@
+<?php
+require_once '../database/ContaRepository.php';
+
+$action = $_GET['action'];
+
+switch($action) {
+    case 'listar':
+        listarContas();
+        break;
+    case 'buscar':
+        buscarContaById();
+        break;
+    case 'cadastrar':
+        cadastrarConta();
+        break;
+    case 'atualizar':
+        atualizarConta();
+        break;
+    case 'excluir': {
+        excluirConta();
+        break;
+    }
+    default:
+        http_response_code(400); // Requisição inválida
+        echo json_encode(['error' => 'Ação inválida']);
+}
+
+function listarContas() {
+    $contas = ContaRepository::getAllContas();
+    echo json_encode($contas);
+}
+
+function buscarContaById() {
+    if($_SERVER['REQUEST_METHOD'] === 'GET') {
+        $id = $_GET['id'];
+        $conta = ContaRepository::getContaById($id);
+
+        if($conta) {
+            echo json_encode($conta);
+        } else {
+            http_response_code(404); // Conta não encontrada
+            echo json_encode(['error' => 'Conta não encontrada']);
+        }
+    } else {
+        http_response_code(405); // Método não permitido
+    }
+}
+
+// (cliente_id, numero, saldo, tipo, limite_cheque_especial, taxa_rendimento)
+// (1, 1001, 1500.00, 'corrente', 500.00, NULL)
+function cadastrarConta() {
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $data = json_decode(file_get_contents("php://input"));
+        
+        $success = ContaRepository::insertConta($data->cliente_id, 
+                                    $data->numero, 
+                                    $data->saldo, 
+                                    $data->tipo, 
+                                    $data->limite_cheque_especial, 
+                                    $data->taxa_rendimento);
+
+        echo json_encode(['success' => $success]);
+    } else {
+        http_response_code(405); // Método não permitido
+    }
+}
+
+function atualizarConta() {
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $data = json_decode(file_get_contents("php://input"));
+        
+        $success = ContaRepository::updateConta($data->conta_id,                                     
+                                    $data->saldo,                                     
+                                    $data->limite_cheque_especial, 
+                                    $data->taxa_rendimento);
+
+        echo json_encode(['success' => $success]);
+    } else {
+        http_response_code(405); // Método não permitido
+    }
+}
+
+function excluirConta() {
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $data = json_decode(file_get_contents("php://input"));
+        $id = $data->id;
+
+        $success = ContaRepository::deleteConta($id);
+        echo json_encode(['success' => $success]);
+    } else {
+        http_response_code(405); // Método não permitido
+    }
+}
+
+?>
---
+++ 18/database/ClienteRepository.php
@@ -0,0 +1,57 @@
+<?php
+require_once 'DatabaseRepository.php';
+
+class ClienteRepository {
+    
+    public static function getAllClientes() {
+        $connection = DatabaseRepository::connect();
+        $result = $connection->query("SELECT * FROM cliente");
+        
+        $clientes = [];
+        if($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $clientes[] = $row;
+            }
+        }
+
+        $connection->close();
+        return $clientes;
+    }
+
+    public static function getClienteById($id) {
+        $connection = DatabaseRepository::connect();
+        $sql = "SELECT * FROM cliente WHERE id = $id";
+        $result = $connection->query($sql);
+
+        $cliente = null;
+        if($result->num_rows > 0) {
+            $cliente = $result->fetch_assoc();
+        }
+        $connection->close();
+        return $cliente;
+    }
+
+    public static function insertCliente($nome, $cpf) {
+        $connection = DatabaseRepository::connect();
+        $sql = "INSERT INTO cliente (nome, cpf) VALUES ('$nome', '$cpf')";
+        $success = $connection->query($sql);
+        $connection->close();
+        return $success;
+    }
+
+    public static function updateCliente($id, $nome, $cpf) {
+        $connection = DatabaseRepository::connect();
+        $sql = "UPDATE cliente SET nome='$nome', cpf='$cpf' WHERE id=$id";
+        $success = $connection->query($sql);
+        $connection->close();
+        return $success;
+    }
+
+    public static function deleteCliente($id) {
+        $connection = DatabaseRepository::connect();
+        $success = $connection->query("DELETE FROM cliente WHERE id=$id");
+        $connection->close();
+        return $success;
+    }
+}
+?>
