b9ffd03adac0bbfae6c4ca8527798d64b80a032e
Walther <wszin06@gmail.com>
2024-04-25 20:16:00 -0300

    19 class produto

--- 19/database/ProdutoRepository.php
+++ 19/database/ProdutoRepository.php
@@ -10,14 +10,27 @@ class ProdutoRepository {
         $result = $connection->query("SELECT * FROM produto");
 
         $produtos = [];
-        if($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
-                $produto = new Produto($row['id'], $row['nome'], $row['descricao'], $row['preco']);
-                $produtos[] = $produto;
+            if($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                    $produto = new Produto($row['id'], $row['nome'], $row['descricao'], $row['preco']);
+                    $produtos[] = $produto;
             }
         }
         $connection->close();
         return $produtos;
+    }
+
+    public static function getProdutosById($id) {
+        $connection = DatabaseRepository::connect();
+        $result = $connection->query("SELECT * FROM produto WHERE id = $id");
+
+        $produto = null;
+        if($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $produto = new Produto($row['id'], $row['nome'], $row['descricao'], $row['preco']);
+        }
+        $connection->close();
+        return $produto;
     }
 }
 
