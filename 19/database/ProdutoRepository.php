747f20d8821143d8803084807d6e9e2d9a4b42ef
WALTER SOUZA DA SILVA <walter.ssilva2@senacsp.edu.br>
2024-04-26 20:06:34 -0300

    19 PRODUTO

--- 19/database/ProdutoRepository.php
+++ 19/database/ProdutoRepository.php
@@ -1,26 +1,24 @@
 <?php
-
 require_once 'DatabaseRepository.php';
 require_once 'model/Produto.php';
-
 
 class ProdutoRepository {
     public static function getAllProdutos() {
         $connection = DatabaseRepository::connect();
         $result = $connection->query("SELECT * FROM produto");
 
-        $produtos = [];
-            if($result->num_rows > 0) {
-                while($row = $result->fetch_assoc()) {
-                    $produto = new Produto($row['id'], $row['nome'], $row['descricao'], $row['preco']);
-                    $produtos[] = $produto;
+        $produtos = [];        
+        if($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $produto = new Produto($row['id'], $row['nome'], $row['descricao'], $row['preco']);
+                $produtos[] = $produto;
             }
         }
         $connection->close();
         return $produtos;
     }
 
-    public static function getProdutosById($id) {
+    public static function getProdutoById($id) {
         $connection = DatabaseRepository::connect();
         $result = $connection->query("SELECT * FROM produto WHERE id = $id");
 
@@ -35,13 +33,38 @@ class ProdutoRepository {
 
     public static function insertProduct(Produto $produto) {
         $connection = DatabaseRepository::connect();
+
         $nome = $produto->getNome();
         $descricao = $produto->getDescricao();
         $preco = $produto->getPreco();
-        $sql = "INSERT INTO produto (nome, descricao, preco) VALUES ('$nome', '$descricao', '$preco')";        
-        $sucess = $connection->query($sql);
-        return $sucess;
+
+        $sql = "INSERT INTO produto (nome, descricao, preco) VALUES ('$nome', '$descricao', '$preco')";
+        $success = $connection->query($sql);
+        $connection->close();
+        return $success;
+    }
+
+
+    public static function updateProduto(Produto $produto) {
+        $connection = DatabaseRepository::connect();
+        $id = $produto->getId();
+        $nome = $produto->getNome();
+        $descricao = $produto->gitDescricao();
+        $preco = $produto->getPreco();
+        $sql = "UPDATE produto SET nome='$nome', descricao='$descricao', preco='$preco'
+            WHERE id=$id";
+        $result = $connection->query($sql);
+        $connection->close();
+        return $success;
     }
 }
+
+    public static function deleteProduto($id) {
+        $connection = DatabaseRepository::connect();
+        $result = $connection->query("DELETE FROM produto WHERE id = $id");
+        $connection->query($result);
+        $connection->close();
+        return $result;
+    }
 
 ?>
--- 19/model/Pedido.php
+++ 19/model/Pedido.php
@@ -1,46 +1,21 @@
 <?php
-
 class Pedido implements JsonSerializable {
     private $id;
     private $data_pedido;
     private $status;
 
-    public function __construct($id, $data_pedido, $status)
-    {
+    public function __construct($id, $data_pedido, $status) {
         $this->id = $id;
         $this->data_pedido = $data_pedido;
         $this->status = $status;
-    }   
+    }
 
-    public function jsonSerialize()
-    {
+    public function jsonSerialize(): array {
         return [
             'id' => $this->id,
             'data_pedido' => $this->data_pedido,
-            'status' => $this->status 
+            'status' => $this->status
         ];
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ?>
